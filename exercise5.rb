03cb4cabea11f2d80d5d43704373a7696adc611f
stephthi <nstephanie2@gmail.com>
2015-10-27 23:20:14 -0400

    Input temperature code

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+# Changing Farenheit to Celsius
+
+puts "What temperature in Farenheit would you like converted to Celsius?"
+	farenIn = gets.chomp.to_i
+
+def tempC(farenIn)
+	return ((farenIn - 32.0) * (5.0/9.0))
+end
+
+farenOut = tempC(farenIn).to_i
+
+puts "#{farenIn} in Farenheit converts to #{farenOut} degrees Celsius."
