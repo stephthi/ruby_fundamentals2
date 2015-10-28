0956437a8743a9304866f4b58aa32690cb9ca716
stephthi <nstephanie2@gmail.com>
2015-10-27 23:52:30 -0400

    Create list with asterisk

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,11 @@
 # Grocery List
 
 grocery_list = ["avocado", "watermelon", "chicken", "mushrooms"]
+
+grocery_list.each do |list_item|
+	puts "* #{list_item}"
+end
+
+# def print_list(list)
+# 	list.each {|list_item| puts "* #{list_item}"}
+# end
